85ed3ec2588afabdf7058ff15cae6823a3a0be72
Jack @ Ubuntu-desk <cybercow.now@gmail.com>
2026-08-24 10:00:36 +0800

    release: wallaby-hires v0.1.9

--- tests/test_version.py
+++ tests/test_version.py
@@ -3,6 +3,6 @@ from wallaby_hires.__main__ import package_version
 
 
 def test_package_and_release_versions_match():
-    assert metadata_version() == "0.1.8"
+    assert metadata_version() == "0.1.9"
     assert package_version() == metadata_version()
-    assert check_version("v0.1.8") == "0.1.8"
+    assert check_version("v0.1.9") == "0.1.9"
